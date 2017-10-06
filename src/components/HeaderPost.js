856a1e48dffdf2278c5e668e757daf4276b6fe60
GabrielePrestifilippo <gabry.501@hotmail.it>
2017-10-07 00:37:58 +0200

    new components for the post

---
+++ src/components/HeaderPost.js
@@ -0,0 +1,78 @@
+import React, { PropTypes } from 'react'
+import { Image, View, TouchableOpacity } from 'react-native'
+import {
+  Container,
+  Content,
+  Card,
+  CardItem,
+  Thumbnail,
+  Text,
+  Button,
+  Icon,
+  List,
+  Left,
+  ListItem,
+  Body,
+  Right
+} from 'native-base'
+import EStyleSheet from 'react-native-extended-stylesheet'
+import { Actions } from 'react-native-router-flux'
+
+export default class HeaderPost extends React.Component {
+
+  constructor (props) {
+    super(props)
+  }
+
+  onClose () {
+    Actions.pop()
+  }
+
+  render () {
+    const img = this.props.img,
+      name = this.props.name
+
+    return (
+      <View>
+          <List style={{backgroundColor:'white', elevation:4, borderBottomWidth:0}}>
+            <ListItem style={{backgroundColor:'white', borderBottomWidth:0}} avatar>
+              <Left>
+                <Thumbnail source={{uri: img}} />
+              </Left>
+              <Body style={{backgroundColor:'white', borderBottomWidth:0}}>
+              <Text>{name}</Text>
+              <Text note>Doing what you like will always keep you happy . .</Text>
+              </Body>
+              <TouchableOpacity style={{backgroundColor:'white'}} button onPress={()=>{this.onClose()}} >
+              <Right style={{backgroundColor:'white', borderBottomWidth:0}}>
+                <Icon name='close' />
+              </Right>
+              </TouchableOpacity>
+            </ListItem>
+          </List>
+
+      </View>
+    )
+  }
+}
+
+HeaderPost.propTypes = {
+  name: PropTypes.string,
+  image: PropTypes.string
+
+}
+const styles = EStyleSheet.create({
+  text: {
+    color: '$textColor',
+    fontSize: 14,
+    textAlign: 'left',
+
+  },
+  thumbnail: {
+    width: 40,
+    height: 100
+  },
+  button: {
+    alignSelf: 'center'
+  }
+})
