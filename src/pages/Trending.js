c045377f951e3de424d44b692bea5bb3a559866d
GabrielePrestifilippo <gabry.501@hotmail.it>
2017-10-07 00:37:16 +0200

    imrpoved trending fro native swiper

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,127 +1,57 @@
-'use strict'
+import React, { Component } from 'react'
+import { Image } from 'react-native'
+import { Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base'
 
-import React, {Component} from 'react'
-import {StyleSheet, Text, View, Image} from 'react-native'
+const cards = [
+  {
+    text: 'Card One',
+    name: 'One',
+    image: {uri:'https://i.vimeocdn.com/portrait/6193893_640x640'},
+  },
+  {
+    text: 'Card Two',
+    name: 'Two',
+    image: {uri:'https://i.vimeocdn.com/portrait/6193893_640x640'},
+  },
 
-import Swiper from 'react-native-deck-swiper'
+]
 
-
-export default class Trending extends React.Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      cards: ['1', '2', '3'],
-      swipedAllCards: false,
-      swipeDirection: '',
-      isSwipingBack: false,
-      cardIndex: 0
-    }
+export default class Trending extends Component {
+  onSwipeRight () {
+    console.log('right')
   }
 
-    renderCard = card => {
-      return (
-        <View style={styles.card}>
-          <Text style={styles.text}>{card}</Text>
-        </View>
-      )
-    };
+  onSwipeLeft () {
+    console.log('left')
+  }
 
-    onSwipedAllCards = () => {
-      this.setState({
-        swipedAllCards: true
-      })
-    };
-
-    swipeBack = () => {
-      if (!this.state.isSwipingBack) {
-        this.setIsSwipingBack(true, () => {
-          this.swiper.swipeBack(() => {
-            this.setIsSwipingBack(false)
-          })
-        })
-      }
-    };
-
-    setIsSwipingBack = (isSwipingBack, cb) => {
-      this.setState(
-        {
-          isSwipingBack
-        },
-        cb
-      )
-    };
-
-    jumpTo = () => {
-      this.swiper.swipeLeft()
-    };
-
-    render() {
-      return (
+  render () {
+    return (
         <View>
-          <Swiper
-            ref={swiper => {
-              this.swiper = swiper
-            }}
-            onSwiped={this.onSwiped}
-            onTapCard={this.jumpTo}
-            cards={this.state.cards}
-            cardIndex={this.state.cardIndex}
-            cardVerticalMargin={80}
-            renderCard={this.renderCard}
-            onSwipedAll={this.onSwipedAllCards}
-            overlayLabels={{
-              bottom: {
-                title: 'BLEAH',
-                swipeColor: '#9262C2',
-                backgroundOpacity: '0.75',
-                fontColor: '#FFF'
-              },
-              left: {
-                title: 'NOPE',
-                swipeColor: '#FF6C6C',
-                backgroundOpacity: '0.75',
-                fontColor: '#FFF'
-              },
-              right: {
-                title: 'LIKE',
-                swipeColor: '#4CCC93',
-                backgroundOpacity: '0.75',
-                fontColor: '#FFF'
-              },
-              top: {
-                title: 'SUPER LIKE',
-                swipeColor: '#4EB8B7',
-                backgroundOpacity: '0.75',
-                fontColor: '#FFF'
-              }
-            }}
-            animateOverlayLabelsOpacity={true}
-            animateCardOpacity={true}
+          <DeckSwiper
+            dataSource={cards}
+            renderItem={item =>
+              <Card style={{elevation: 3}}>
+                <CardItem>
+                  <Left>
+                    <Thumbnail source={item.image} />
+                    <Body>
+                    <Text>{item.text}</Text>
+                    <Text note>NativeBase</Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                <CardItem cardBody onSwipeRight={() => {this.onSwipeRight()}}>
+                  <Image style={{height: 300, flex: 1}} source={item.image} />
+                </CardItem>
+                <CardItem>
+                  <Icon name="heart" style={{color: '#ED4A6A'}} />
+                  <Text>{item.name}</Text>
+                </CardItem>
+              </Card>
+            }
           />
         </View>
-      )
-    }
-}
-
-const styles = StyleSheet.create({
-  card: {
-    flex: 1,
-    borderRadius: 4,
-    borderWidth: 2,
-    borderColor: '#E8E8E8',
-    justifyContent: 'center',
-    backgroundColor: 'white'
-  },
-  text: {
-    textAlign: 'center',
-    fontSize: 50,
-    backgroundColor: 'transparent'
-  },
-  done: {
-    textAlign: 'center',
-    fontSize: 30,
-    color: 'white',
-    backgroundColor: 'transparent'
+    )
   }
-})
+}
