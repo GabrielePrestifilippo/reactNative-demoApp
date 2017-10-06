4fe3f2cdfbf015fe0b002e16733954e28bd6105e
GabrielePrestifilippo <gabry.501@hotmail.it>
2017-10-06 17:11:18 +0200

    stack for nested scene

--- App.js
+++ App.js
@@ -1,37 +1,46 @@
 import React from 'react'
-import { AppRegistry, Text, View, Button, Linking } from 'react-native'
-import { createStore } from 'redux'
+import {AppRegistry, Text, View, Button, Linking} from 'react-native'
+import {createStore} from 'redux'
 import {
   Scene,
   Router,
   Actions,
   Reducer,
   ActionConst,
+  Stack
 } from 'react-native-router-flux'
 
-import { appReducer } from './src/reducers'
-import {Provider, connect} from 'react-redux';
+import {appReducer} from './src/reducers'
+import {Provider, connect} from 'react-redux'
 import Profile from './src/pages/Profile'
 import Influencers from './src/pages/Influencers'
 import Trending from './src/pages/Trending'
-import Influencer from './src/pages/Influencer'
+import InfluencerPosts from './src/pages/InfluencerPosts'
 import Login from './src/pages/Login'
+import EStyleSheet from 'react-native-extended-stylesheet'
 
+
+EStyleSheet.build({
+  $textColor: '#0275d8'
+})
 const store = createStore(appReducer)
 
 const myInfluencers = () => {
   return (
     <Provider store={store}>
       <Router wrapBy={connect()}>
-        <Scene key="root" hideNavBar hideTabBar>
-          <Scene key="tabbar" hideNavBar tabs={true}  tabBarPosition="bottom">
-            <Scene key='Influencers' title='AppList' component={Influencers}></Scene>
-            <Scene key='Profile' title='AppList' component={Profile}></Scene>
-            <Scene key='Trending' title='AppList' component={Trending}></Scene>
+        <Scene key="root" hideNavBar={true} hideTabBar={true}>
+          <Scene key="tabbar" hideNavBar={true} tabs={true} tabBarPosition="bottom">
+            <Stack key='Influencers'>
+              <Scene key='InfluencersList' title='AppList' component={Influencers}/>
+              <Scene key='InfluencerPosts' title='AppList' component={InfluencerPosts}/>
+            </Stack>
+            <Scene key='Profile' title='AppList' component={Profile}/>
+            <Scene key='Trending' title='AppList' component={Trending}/>
           </Scene>
-          <Scene key="stack" hideNavBar hideTabBar >
-            <Scene key='Influencer' title='AppList' component={Influencer}></Scene>
-            <Scene key='Login' title='AppList' component={Login}></Scene>
+
+          <Scene key="stack" hideNavBar={false} hideTabBar={false}>
+            <Scene key='Login' title='AppList' component={Login}/>
           </Scene>
 
         </Scene>
