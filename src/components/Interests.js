266e0fea17c08af2aa80af5b8a5d7be4f59a9c1d
GabrielePrestifilippo <gabry.501@hotmail.it>
2017-10-08 20:17:38 +0200

    extended intesrest page

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,24 +1,28 @@
 import React from 'react'
-import {Text, View, Button, StyleSheet} from 'react-native'
+import { Text, View, Button, StyleSheet } from 'react-native'
+import {
+  Icon,
+} from 'native-base'
 
 export default class Interests extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
   }
 
-  render() {
+  render () {
     var self = this
     return <View style={styles.interests}>
       {this.props.data.map(function (key, val) {
         return <View key={val} style={styles.interest}>
           <Text style={styles.interestText}>{key}</Text>
           <Text onPress={() => {
-            self.props.data.splice(self.props.data.indexOf(key),1)
+            self.props.data.splice(self.props.data.indexOf(key), 1)
             self.setState({data: self.props.data}) //update state
-
           }}
-            style={styles.deleteButton}
-          >x</Text>
+                style={styles.deleteButton}
+          >
+            <Icon style={styles.icon} name='close' />
+          </Text>
         </View>
       })}
     </View>
@@ -31,27 +35,29 @@ const styles = StyleSheet.create({
     position: 'relative',
     justifyContent: 'flex-start',
     flexDirection: 'row',
-    flexWrap:'wrap',
-    flex:1
+    flexWrap: 'wrap',
+    flex: 1
   },
   interest: {
-    backgroundColor: '#98e7ff',
-    borderColor: '#1459ff',
+    backgroundColor: '#007ced',
+    borderColor: '#007ced',
     borderWidth: 1,
-    borderRadius: 5,
+    borderRadius: 2,
     margin: 5,
-    height:30,
+    height: 45,
     flexDirection: 'row',
-    justifyContent: 'flex-start'
+    justifyContent: 'flex-start',
+    padding: 10
 
   },
   interestText: {
-    color: 'white',
-    padding: 5
+    color: '#fbfbfb',
+    fontSize: 16,
   },
   deleteButton: {
-    color: '#777a78',
+    color: '#fbfbfb',
     padding: 5
-  }
+  },
+  icon: {fontSize: 16, paddingLeft: 2, color: 'white'}
 
 })
