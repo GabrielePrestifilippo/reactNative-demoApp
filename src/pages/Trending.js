5a0fd88c7bd594b6ff1daaa3e659f07842956b8b
GabrielePrestifilippo <gabry.501@hotmail.it>
2017-10-08 20:18:20 +0200

    new trending page

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react'
-import { Image } from 'react-native'
-import { Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base'
+import { Image, Modal, Dimensions, TouchableHighlight } from 'react-native'
+import {
+  Container,
+  Header,
+  View,
+  Button,
+  DeckSwiper,
+  Card,
+  CardItem,
+  Item,
+  Label,
+  Thumbnail,
+  Text,
+  Left,
+  Body,
+  Right,
+  Icon,
+  Content,
+  Footer
+} from 'native-base'
+import EStyleSheet from 'react-native-extended-stylesheet'
 
+var {height, width} = Dimensions.get('window')
 const cards = [
   {
     text: 'Card One',
     name: 'One',
-    image: {uri:'https://i.vimeocdn.com/portrait/6193893_640x640'},
+    like: 33,
+    follow: 33,
+    image: {uri: 'https://i.vimeocdn.com/portrait/6193893_640x640'},
   },
   {
     text: 'Card Two',
     name: 'Two',
-    image: {uri:'https://i.vimeocdn.com/portrait/6193893_640x640'},
+    like: 11,
+    follow: 53,
+    image: {uri: 'https://i.vimeocdn.com/portrait/6193893_640x640'},
   },
 
 ]
@@ -27,31 +51,63 @@ export default class Trending extends Component {
 
   render () {
     return (
-        <View>
-          <DeckSwiper
-            dataSource={cards}
-            renderItem={item =>
-              <Card style={{elevation: 3}}>
-                <CardItem>
-                  <Left>
-                    <Thumbnail source={item.image} />
-                    <Body>
-                    <Text>{item.text}</Text>
-                    <Text note>NativeBase</Text>
-                    </Body>
-                  </Left>
-                </CardItem>
-                <CardItem cardBody onSwipeRight={() => {this.onSwipeRight()}}>
-                  <Image style={{height: 300, flex: 1}} source={item.image} />
-                </CardItem>
-                <CardItem>
+      <View style={{flex: 1}}>
+        <DeckSwiper
+          style={{flex: 1, position: 'relative', flexDirection: 'row'}}
+          dataSource={cards}
+          looping={false}
+          onSwipeLeft={() => {this.onSwipeLeft()}}
+          onSwipeRight={() => {this.onSwipeRight()}}
+          renderItem={item =>
+            <Card style={{elevation: 3}}>
+              <CardItem style={{flex: 1}}>
+                <Left>
+                  <Thumbnail source={item.image} />
+                  <Body>
+                  <Text>{item.text}</Text>
+                  <Text note>NativeBase</Text>
+                  </Body>
+                </Left>
+                <Right>
+                  <Button style={{position: 'relative'}}>
+                    <Icon name='people' />
+                    <Text>Follow</Text>
+                  </Button>
+                </Right>
+              </CardItem>
+              <CardItem style={{flex: 1}} cardBody>
+                <Image style={{flex: 1, height: height - 295}} resizeMode='cover' source={item.image} />
+              </CardItem>
+              <CardItem style={styles.twoSide}>
+                <View style={styles.left}>
                   <Icon name="heart" style={{color: '#ED4A6A'}} />
-                  <Text>{item.name}</Text>
-                </CardItem>
-              </Card>
-            }
-          />
-        </View>
+                  <Text>{item.like}</Text>
+                </View>
+                <View style={styles.right}>
+                  <Icon name="people" style={{color: '#007ced'}} />
+                  <Text>{item.follow}</Text>
+                </View>
+              </CardItem>
+
+            </Card>
+          }
+        />
+      </View>
     )
   }
 }
+
+const styles = EStyleSheet.create({
+  left: {
+    alignSelf: 'flex-start',
+    paddingLeft: '20%',
+  },
+  right: {
+    alignSelf: 'flex-end',
+    paddingRight: '20%',
+  },
+  twoSide: {
+    justifyContent: 'space-between',
+    flex: 1
+  }
+})
